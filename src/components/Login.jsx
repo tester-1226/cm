e750a8967f9d6934ccb450336dbfcc4e4cb860eb
ugajtate <104660702+ugajtate@users.noreply.github.com>
2024-02-10 08:42:52 -0500

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../css/form.css';
 
 const Login = (props) => {
     return (
-        <div></div>
+        <div>
+            <div class = "form-section">
+                <div class = "form-input-wrapper">
+                    <input class="form-input" type="text" placeholder="Email" name="email"></input>
+                    <span class = "form-input-focus-border"></span>
+                    <span class = "form-input-focus-border-reverse"></span>
+                </div>
+            </div>
+            <div class = "form-section">
+                <div class = "form-input-wrapper">
+                    <input class="form-input" type="text" placeholder="Password" name="email"></input>
+                    <span class = "form-input-focus-border"></span>
+                    <span class = "form-input-focus-border-reverse"></span>
+                </div>
+            </div>
+            <div class = "form-section">
+                    <button class = "form-large-button">Login</button>
+            </div>
+        </div>
     );
 }
 
