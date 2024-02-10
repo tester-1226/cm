51e3c9f8dc9046de0de49f8e76b83d60193ef7be
ugajtate <104660702+ugajtate@users.noreply.github.com>
2024-02-10 16:52:03 -0500

    startingAdd

--- src/components/AddEvent.jsx
+++ src/components/AddEvent.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
-
+import '../css/addEvent.css';
 const AddEvent = (props) => {
     return (
-        <div>
-            <div>
-                <form>
-                    <label for="img">Event Picture:</label>
-                    <input type="file" id="img" name="img" accept="image/*"></input>
-                    <input type="submit"></input>
-                </form>
-            </div>
-            <div>
-                <form>
-                    <br>
-                        <label for="firstName">Event name:</label>
-                        <input type="text" id="eventName" name="eventName"></input>
-                    </br>
-                    <br>
-                    <label for="freeform">Description of Event:</label>
-                    </br>
-                    <br>
-                    <textarea id="freeform" name="freeform" rows="4" cols="50">
-                    </textarea>
-                    </br>
-                    <br>
-                        <label for="firstName">Event Date:</label>
-                        <input type="date" name="eventDate"></input>
-                    </br>
-                    <br>
-                        <label for="firstName">Event Time:</label>
-                        <input type="time" name="eventTime"></input>
-                    </br>
-                    <input type="submit" value="Create Event"> </input>
-                </form>             
-            </div>
-        </div>
-
+      <div>
+        
+      </div>
     );
 }
-
 export default AddEvent;
