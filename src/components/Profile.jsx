3cc53146ebad8fdbfb4731cd0d6341cbb5635530
Pranay U <pranayudhaya@gmail.com>
2024-02-10 15:47:14 -0500

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
 import '../css/profile.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { genericFetch } from './datafetch';
+import { signInWithEmailAndPassword, signOut, sendEmailVerification, getAuth } from 'firebase/auth';
+import { database } from '../firebase_setup/firebase.js'
+import { ref, push, child, update } from "firebase/database";
+import { auth } from '../firebase_setup/firebase';
+import Cookies from 'js-cookie';
 
 const Profile = (props) => {
+    const user = Cookies.get('token');
+    console.log(user);
+
+    const dbRef = ref(getDatabase());
+
+    const userID = user.uid;
+    console.log(userID);
+
+
     return (
         <div>
            <div align = "center">
