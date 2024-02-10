7f18813c13118f78bdfe658caa4d040e3fa9ae89
ugajtate <104660702+ugajtate@users.noreply.github.com>
2024-02-10 10:04:06 -0500

    Skulls

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -31,7 +31,6 @@ const Profile = (props) => {
                             </tr>
                         </div>
                         <div>
-                            
                         </div>
                     </div>
                 </div>
