aa149ec0f5db5ded72d7f7524b1dab2244437fcc
Pranay U <pranayudhaya@gmail.com>
2024-02-10 11:34:59 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import { genericFetch } from './datafetch';
 import '../css/form.css';
+import { signInWithEmailAndPassword, signOut, sendEmailVerification } from 'firebase/auth';
+import { database } from '../../firebase_setup/firebase.js'
+import { ref, push, child, update } from "firebase/database";
+import { auth } from '../../firebase_setup/firebase';
+
 
 const Login = (props) => {
     const navigate = useNavigate();
@@ -13,15 +18,36 @@ const Login = (props) => {
     const [redirectHome, setRedirectHome] = useState(false);
 
     const handleFormSubmit = (e) => {
-        e.preventDefault();
+        /*e.preventDefault();
         genericFetch(
             "GET",
             "http://localhost:8080/api/v1/customer/info?email="+username,
             {}
         ).then(resp => {
             
-        })
-    }
+        })*/
+
+        signInWithEmailAndPassword(auth, username, password)
+            .then((userCredential) => {
+                // Signed in
+                /*var lgDate = new Date();
+                update(ref(database, 'users/' + user.uid), {
+                    last_login: lgDate,
+                })*/
+
+                const user = userCredential.user;
+                props.changeLoginState(true, user.uid)
+                message.success("Signed in as " + user.email)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                //message.error(errorMessage)
+                console.log(errorCode, errorMessage);
+                message.error("Invalid email or password!");
+            });
+        }
+
 
     return (
         <div>
