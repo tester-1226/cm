407ae016b7204539a0a7381fe4f5faff264d30dd
Kavin Parikh <kpk1611@gmail.com>
2024-02-10 23:43:36 -0500

    data works

--- src/components/AddEvent.jsx
+++ src/components/AddEvent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../css/addEvent.css'
 import Navbar from './Navbar';
 import { db } from '../firebase_setup/firebase.js'
@@ -7,99 +7,139 @@ import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "
 import { message, Form, Input, Checkbox, Button } from 'antd';
 import { useNavigate } from "react-router-dom";
 
-const AddEvent = (props) => {
 
+const AddEvent = () => {
     const navigate = useNavigate();
-    const handleSubmit = (e) => {
-        let obj = {
-            uid: '',
-            description: e.description,
-            name: e.name,
-            location: e.location,
-            date: e.date,
-            time: e.time
-        }
-    }
+    const [eventData, setEventData] = useState({
+        description: '',
+        name: '',
+        location: '',
+        date: '',
+        time: ''
+    });
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEventData({ ...eventData, [name]: value });
+    };
+
+    const handleSubmit = () => {
+        const eventsRef = ref(db, 'events');
+        push(eventsRef, eventData)
+            .then(() => {
+                message.success("Event added successfully!");
+                navigate('/'); // Redirect to homepage after adding event
+            })
+            .catch((error) => {
+                console.error("Error adding event:", error);
+                message.error("Failed to add event. Please try again.");
+            });
+    };
+
     return (
-        <div align = "center">
-            <h1>
-                Create An Event
-            </h1>
-            <div>
-                <label for="eventName">Event name:</label>
-                <input type="text" id="eventName" name="eventName"></input>
-            </div>
-            <br>
-            </br>
-            <div>
-                <label for="location">Location:</label>
-                <input type="text" id="eventLocation" name="eventLocation"></input>
-            </div>
-            <br>
-            </br>
-            <div>
-                <label for="userName">Username:</label>
-                <input type="text" id="userName" name="userName"></input>
-                <label for="date">Date:</label>
-                <input type="date" name="eventDate"></input>
-                <label for="time">Time:</label>
-                <input type="time" name="eventDate"></input>
-            </div>
-            <br>
-            </br>
-            <div>
-                <label>
-                Event Description
-                </label>
-                <br>
-                </br>
-                <textarea rows="4" cols="50" class="form-resize">
+        <div>
+            <Form onFinish={handleSubmit}>
+                <Form.Item label="Description">
+                    <Input type="text" name="description" value={eventData.description} onChange={handleChange} />
+                </Form.Item>
+                <Form.Item label="Name">
+                    <Input type="text" name="name" value={eventData.name} onChange={handleChange} />
+                </Form.Item>
+                <Form.Item label="Location">
+                    <Input type="text" name="location" value={eventData.location} onChange={handleChange} />
+                </Form.Item>
+                <Form.Item label="Date">
+                    <Input type="date" name="date" value={eventData.date} onChange={handleChange} />
+                </Form.Item>
+                <Form.Item label="Time">
+                    <Input type="time" name="time" value={eventData.time} onChange={handleChange} />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">Create Event</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
 
-                </textarea>
-            </div>
-            <br>
-            </br>
-            <div align = "center">
-                <button class="form-createEvent-button">Create Event</button>
-             </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // return (
+    //     <div align = "center">
+    //         <h1>
+    //             Create An Event
+    //         </h1>
+    //         <div>
+    //             <label for="eventName">Event name:</label>
+    //             <input type="text" id="eventName" name="eventName"></input>
+    //         </div>
+    //         <br>
+    //         </br>
+    //         <div>
+    //             <label for="location">Location:</label>
+    //             <input type="text" id="eventLocation" name="eventLocation"></input>
+    //         </div>
+    //         <br>
+    //         </br>
+    //         <div>
+    //             <label for="userName">Username:</label>
+    //             <input type="text" id="userName" name="userName"></input>
+    //             <label for="date">Date:</label>
+    //             <input type="date" name="eventDate"></input>
+    //             <label for="time">Time:</label>
+    //             <input type="time" name="eventDate"></input>
+    //         </div>
+    //         <br>
+    //         </br>
+    //         <div>
+    //             <label>
+    //             Event Description
+    //             </label>
+    //             <br>
+    //             </br>
+    //             <textarea rows="4" cols="50" class="form-resize">
+
+    //             </textarea>
+    //         </div>
+    //         <br>
+    //         </br>
+    //         <div align = "center">
+    //             <button class="form-createEvent-button">Create Event</button>
+    //          </div>
 
             
-        </div>
+    //     </div>
 
-    );
-}
+    // );
 export default AddEvent;
